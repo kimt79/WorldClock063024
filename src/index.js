ed3fefee69e8a1ebcf526ac6958b131b56a1c77b
kimt79 <164622332+kimt79@users.noreply.github.com>
2024-07-02 07:31:31 -0500

    created js/current date/time for displayed cities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert("hello");
+function updateKansasCityTime() {
+  let kansasCityElement = document.querySelector("#kansas-city");
+  let kansasCityDateElement = kansasCityElement.querySelector(".date");
+  let kansasCityTimeElement = kansasCityElement.querySelector(".time");
+  let kansasCityTime = moment().tz("America/Chicago");
+
+  kansasCityDateElement.innerHTML = kansasCityTime.format("MMMM DO YYYY");
+  kansasCityTimeElement.innerHTML = kansasCityTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+
+setInterval(updateKansasCityTime);
+
+function updateParisTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM DO YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>]A[</small>]");
+}
+
+setInterval(updateParisTime, 1000);
